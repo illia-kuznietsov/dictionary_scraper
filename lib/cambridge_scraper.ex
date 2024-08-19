54572afa7aa9b4bc2e93b14b7cc3c2638056bc8d
illia-kuznietsov <illia.kuzn.work@gmail.com>
2024-08-19 16:38:59 +0300

    now gets a word definition out of url (in case the internet connection is alright)

--- lib/cambridge_scraper.ex
+++ lib/cambridge_scraper.ex
@@ -3,16 +3,29 @@ defmodule CambridgeScraper do
   Documentation for `CambridgeScraper`.
   """
 
-  @doc """
-  Hello world.
+  def scrape(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        parse_html(body)
 
-  ## Examples
+      {:ok, %HTTPoison.Response{status_code: status_code}} ->
+        {:error, "Failed to fetch page. Status code: #{status_code}"}
 
-      iex> CambridgeScraper.hello()
-      :world
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, "HTTP request failed: #{reason}"}
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defp parse_html(html) do
+    # Parse the HTML body
+    {:ok, document} = Floki.parse_document(html)
+
+    titles = Floki.find(document, "div.di-title")
+    definitions = Floki.find(document, "div.def.ddef_d.db")
+
+    Enum.zip(titles, definitions)
+    |> Enum.map(fn {title, definition} ->
+      %{title: Floki.text(title), definition: Floki.text(definition)}
+    end)
   end
 end
--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule CambridgeScraper.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 2.0"},
+      {:floki, "~> 0.36.2"},
+      {:credo, "~> 1.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
