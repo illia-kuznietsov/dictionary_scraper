e846e5dad76d930829831b242cfbf1db6b48d144
Illia Kuznietsov <illia.kuzn.work@gmail.com>
2024-09-04 12:27:39 +0300

    now can search by the word

--- lib/cambridge_scraper.ex
+++ lib/cambridge_scraper.ex
@@ -2,8 +2,11 @@ defmodule CambridgeScraper do
   @moduledoc """
   Documentation for `CambridgeScraper`.
   """
+  @url "https://dictionary.cambridge.org/dictionary/english/"
 
-  def scrape(url) do
+  def search(word) when is_binary(word), do: scrape(@url <> word)
+  
+  defp scrape(url) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         parse_html(body)
@@ -21,31 +24,31 @@ defmodule CambridgeScraper do
     {:ok, document} = Floki.parse_document(html)
 
     titles = Floki.find(document, "div.di-title")
-
     definitions =
       Floki.find(document, "div.def.ddef_d.db")
-      |> Floki.traverse_and_update(fn
-        {"div", params, children} ->
-          {"div", params,
-           [
-             children
-             |> Enum.map_join(" ",
-               &case &1 do
-                 {"a", _, text} -> Enum.join(text, " ") |> String.trim()
-                 text when is_binary(text) -> String.trim(text)
-               end
-             )
-           ]}
+      # |> Floki.traverse_and_update(fn
+      #   {"div", params, children} ->
+      #     {"div", params,
+      #      [
+      #        children
+      #        |> Enum.map_join(" ",
+      #          &case &1 do
+      #            {"a", _, text} -> Enum.join(text, " ") |> String.trim()
+      #            {"span", _, [{"a", _, text}]} -> Enum.join(text, " ") |> String.trim()
+      #            text when is_binary(text) -> String.trim(text)
+      #          end
+      #        )
+      #      ]}
 
-        other ->
-          other
-      end)
+      #   other ->
+      #     other
+      # end)
 
     Enum.zip(titles, definitions)
     |> Enum.map(fn {title, definition} ->
       %{
         title: title |> Floki.text(),
-        definition: definition |> Floki.text()
+        definition: definition |> Floki.text(sep: " ") |> String.replace(~r/\s+/, " ")
       }
     end)
   end
